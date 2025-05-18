747e1a3d35d013e82f5d03e09116b640236589b4
mikhail-karpov <mikhailkarpov@hotmail.com>
2025-05-18 23:23:49 +0400

    feat (api-gateway, chat-service): Send traces to Zipkin

---
+++ api-gateway/src/main/java/com/mikhailkarpov/apigateway/filters/TracingFilter.java
@@ -0,0 +1,47 @@
+package com.mikhailkarpov.apigateway.filters;
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.TraceContext;
+import io.micrometer.tracing.Tracer;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class TracingFilter implements GlobalFilter, Ordered {
+
+  private static final Logger log = LoggerFactory.getLogger(TracingFilter.class);
+
+  private final Tracer tracer;
+  private final String headerName = "Trace-ID";
+
+  public TracingFilter(@Autowired Tracer tracer) {
+    this.tracer = tracer;
+  }
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+    log.info("Entering tracing filter. Span: {}", tracer.currentSpan());
+
+    Optional.ofNullable(tracer.currentSpan())
+        .map(Span::context)
+        .map(TraceContext::traceId)
+        .ifPresent(traceId -> exchange.getResponse().getHeaders().add(headerName, traceId));
+
+    return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return -1;
+  }
+
+}
