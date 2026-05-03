b63563cb4db8db6e237f691ddb33dda81655e695
karpovmi <karpovmi@skblab.ru>
2026-05-03 19:05:23 +0400

    feat: logs and traces with Grafana Alloy, Loki & Tempo

---
+++ api-gateway/src/main/java/com/mikhailkarpov/apigateway/config/ContextPropagationConfiguration.java
@@ -0,0 +1,14 @@
+package com.mikhailkarpov.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
+
+@Configuration
+public class ContextPropagationConfiguration {
+
+  @Bean
+  ContextPropagatingTaskDecorator contextPropagatingTaskDecorator() {
+    return new ContextPropagatingTaskDecorator();
+  }
+}
